31572ea9149b1a2492f188552d48efa8f1bd94cc
agentero28 <85558291+agentero28@users.noreply.github.com>
2021-06-11 11:43:48 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -77,3 +77,34 @@ $(function () {
     });
 
 });
+
+
+/* javascript for sticky header */
+
+window.addEventListener('scroll',()=>{
+	const text = document.querySelectorAll('.menu_knop')
+	if(pageYOffset > 95 ){
+		document.querySelector('#nav_animation').style.backgroundColor="#000"
+		text.forEach(ele=>{
+			ele.style.color="#fff"
+		})
+		document.querySelector("#menu").style.backgroundColor="#000"
+	}else{
+		document.querySelector('#nav_animation').style.backgroundColor="#fff"
+		text.forEach(ele=>{
+			ele.style.color="#000"
+		})
+		document.querySelector("#menu").style.backgroundColor="#fff"
+	}
+})
+
+const btn = document.querySelector('.hamburger-menu');
+btn.addEventListener('click',()=>{
+	document.querySelector('#menu').style.right="0%"
+
+})
+
+const cross = document.querySelector('.cross')
+cross.addEventListener('click',()=>{
+	document.querySelector('#menu').style.right="-100%"
+})
